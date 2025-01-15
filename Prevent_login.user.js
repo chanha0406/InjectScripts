fcf76bab7487df8d25939eb94486f977fe7d6832
chanha-ryu <chanha.ryu@samsung.com>
2025-01-15 14:40:01 +0900

    Change name

--- Prevent_login.user.js
+++ Prevent_login.user.js
@@ -5,8 +5,8 @@
 // @match        https://m.fmkorea.com/*
 // @match        https://www.fmkorea.com/*
 // @grant        none
-// @updateURL https://raw.githubusercontent.com/chanha0406/InjectScripts/refs/heads/master/Prevent_login.js
-// @downloadURL https://raw.githubusercontent.com/chanha0406/InjectScripts/refs/heads/master/Prevent_login.js
+// @updateURL https://raw.githubusercontent.com/chanha0406/InjectScripts/refs/heads/master/Prevent_login.user.js
+// @downloadURL https://raw.githubusercontent.com/chanha0406/InjectScripts/refs/heads/master/Prevent_login.user.js
 // ==/UserScript==
 
 (function () {
