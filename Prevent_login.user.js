23e6d6bed7acd8aba49f3c190417e377e480fd73
chanha-ryu <chanha.ryu@samsung.com>
2025-01-16 18:03:17 +0900

    Upadte Version

--- Prevent_login.user.js
+++ Prevent_login.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Prevent login script
-// @version      2025-01-16
+// @version      2025-01-16.2
 // @description  Remove login and logo + etc
 // @match        https://m.fmkorea.com/*
 // @match        https://www.fmkorea.com/*
