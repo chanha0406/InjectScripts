8a474a0902673e05a1b3ed5f0d1d7d9da4fdb09a
Chan-Ha Ryu <chanha0406@gmail.com>
2025-01-24 01:56:50 +0900

    Fix run-at

--- Prevent_login.user.js
+++ Prevent_login.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Prevent login script
-// @version      1.6
+// @version      1.7
 // @description  Remove login and logo + etc
 // @match        https://m.fmkorea.com/*
 // @match        https://www.fmkorea.com/*
 // @grant        none
-// @run-at  document-start
+// @run-at  document-end
 // @updateURL https://raw.githubusercontent.com/chanha0406/InjectScripts/master/Prevent_login.user.js
 // @downloadURL https://raw.githubusercontent.com/chanha0406/InjectScripts/master/Prevent_login.user.js
 // ==/UserScript==
